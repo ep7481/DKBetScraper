263b9f91406edf9fef7654768ef940953437c2df
Evan Paules <ep7481@ship.edu>
2023-06-19 15:28:54 -0400

    Added functionality for the bet to be put into a mysql database instead of the excel file so we can get it easier on the site.

--- DKScraper.py
+++ DKScraper.py
@@ -1,9 +1,8 @@
 import random
+import mysql.connector
+from mysql.connector import Error
 from draftking_class import DraftKings
-from xlwt import Workbook
 
-wb = Workbook()
-sheet1 = wb.add_sheet('Sheet 1')
 gamelist = []
 dk = DraftKings(league="MLB")
 
@@ -15,13 +14,43 @@ for game, event_id in game_ids.items():
     print(game, event_id)
     gamelist.append(game)
 
-team1 = random.choice(gamelist).split("@")
-sheet1.write(0,0,team1[0])
-sheet1.write(1,0,team1[1])
-wb.save('Bets.xls')
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        database='bets_db',
+        user='root',
+        password=''
+    )
 
+    if connection.is_connected():
+        print('Connected to MySQL database')
+except Error as e:
+    print('Error while connecting to MySQL', e)
+
+cursor = connection.cursor()
+
+delete_query = "DELETE FROM bets"
+cursor.execute(delete_query)
+print("Previous data deleted successfully")
+
+team1 = random.choice(gamelist).split("@")
+team2 = team1[0]
+team3 = team1[1]
+
+
+query = "INSERT INTO bets (Teamone, Teamtwo) VALUES (%s, %s)"
+
+data = (team2, team3)
+cursor.execute(query, data)
+
+
+connection.commit()
+
+cursor.close()
+connection.close()
 """
 Updates for the moneylines only
 """
 dk.live_odds_stream(
     event_ids=["28335346", "28335344", "28335347"], markets=['Moneyline'])
+
